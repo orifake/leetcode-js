4394b85c42ac70de6d70c7ae223bc9db771b007a
Yujingyi <349758699@qq.com>
2016-06-28 00:41:35 +0800

    create Binary Tree Paths

---
+++ Binary Tree Paths.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    var result = [];
+    
+    getPath(root, "", result);
+    
+    return result;
+};
+
+function getPath(node, curVal, result) {
+    if (node === null) {
+        return;
+    }
+    
+    if (!curVal) {
+        curVal = '' + node.val;
+    } else {
+        curVal += '->' + node.val;
+    }
+    
+    if (node.left) {
+        getPath(node.left, curVal, result);
+    }
+    
+    if (node.right) {
+        getPath(node.right, curVal, result);
+    }
+    
+    if (!node.left && !node.right) {
+        result.push(curVal);
+    }
+}
