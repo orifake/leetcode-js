ea59d7a94db4098426db86fe0b228904d305ed8f
Yujingyi <349758699@qq.com>
2016-07-03 22:07:06 +0800

    create Sqrt(x)

---
+++ Sqrt(x).js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    var start = 1,
+        end = x,
+        mid;
+    if (x === 0) {
+        return 0;
+    }
+
+    while (start + 1 < end) {
+        mid = start + parseInt((end - start) / 2);
+        if (mid * mid <= x) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+
+    return start
+};
