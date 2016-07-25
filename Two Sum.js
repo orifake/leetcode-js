bcdfa0e8a76e4a6195176c69763d00d508ed5440
Yujingyi <349758699@qq.com>
2016-07-25 23:26:51 +0800

    create Two Sum

---
+++ Two Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var i, j, length = nums.length;
+    for (i = 0; i < length; i++) {
+        for (j = i + 1; j < length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
