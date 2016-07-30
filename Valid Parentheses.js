c9cf989437cff723778c1fe7d50249d02abe018b
Yujingyi <349758699@qq.com>
2016-07-30 23:36:05 +0800

    create Valid Parentheses

---
+++ Valid Parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var stack = [];
+    var pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for (var i = 0; i < s.length; i++) {
+        if (!stack.length) {
+            stack.push(s[i])
+            return
+        }
+        if (s[i] === pairs[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+        console.log(stack);
+    }
+
+    return !stack.length;
+};
+
+isValid('()');
