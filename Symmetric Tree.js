86f86b162112167c1313bc7df164ecf1bfe60762
Yujingyi <349758699@qq.com>
2016-08-08 22:53:00 +0800

    create Symmetric Tree

---
+++ Symmetric Tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root)
+        return true;
+
+    return isMirror(root.left, root.right);
+};
+
+var isMirror = function(a, b) {
+    if (!a && !b) {
+        return true;
+    }
+    if (!a || !b) {
+        return false
+    }
+    return (a.val === b.val) && isMirror(a.left, b.right) && isMirror(a.right, b.left);
+}
