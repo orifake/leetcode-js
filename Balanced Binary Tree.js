c2e69af246ade3c049b865fb9f59040198ef0d02
orifake <349758699@qq.com>
2022-06-01 00:12:45 +0800

    update:Balanced Binary Tree

--- Balanced Binary Tree.js
+++ Balanced Binary Tree.js
@@ -9,20 +9,21 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
-    var flag = true;
-    var check = function(root) {
-        if (!root) return;
 
-        var a = root.left ? check(root.left) : 0,
-            b = root.right ? check(root.right) : 0;
-
-        if (Math.abs(a - b) > 1) {
-            flag = false;
-        }
-
-        return Math.max(a, b) + 1;
-    };
-    check(root);
-    return flag;
+/**
+ *  Time: O(n)
+ *  Space: O(1)
+ */
+const isBalanced = function (root) {
+  if (!root) return ture;
+  if (Math.abs(depth(root.left) - depth(root.right)) <= 1) {
+    return isBalanced(root.left) && isBalanced(root.right)
+  } else {
+    return false
+  }
 };
+
+const depth = (node) => {
+  if (!node) return 0;
+  return Math.max(depth(node.left), depth(node.right)) + 1
+}
