f08157489f5e442982faf0571495b85b8d8f595d
orifake <349758699@qq.com>
2022-06-04 00:19:57 +0800

    update:Best Roman to integer

--- Roman to Integer.js
+++ Roman to Integer.js
@@ -1,31 +1,35 @@
 /**
+ *  Time: O(n)
+ *  Space: O(1)
+ */
+
+/**
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    var hash = {
-        I: 1,
-        X: 10,
-        C: 100,
-        M: 1000,
-        V: 5,
-        L: 50,
-        D: 500
-    };
+const romanToInt = function (s) {
+  const hash = {
+    I: 1,
+    X: 10,
+    C: 100,
+    M: 1000,
+    V: 5,
+    L: 50,
+    D: 500
+  };
 
-    var sum = 0;
-    var roman, nextRoman;
+  let sum = 0;
+  let roman, nextRoman;
 
-    for (var i = 0, len = s.length; i < len; i++) {
-        roman = hash[s[i]];
-        nextRoman = i + 1 === len ? 0 : hash[s[i + 1]];
+  for (let i = 0, len = s.length; i < len; i++) {
+    roman = hash[s[i]];
+    nextRoman = i + 1 === len ? 0 : hash[s[i + 1]];
 
-        if (nextRoman > roman) {
-            sum += nextRoman - roman;
-            i++;
-        } else
-            sum += roman;
-    }
-
-    return sum;
+    if (nextRoman > roman) {
+      sum += nextRoman - roman;
+      i++;
+    } else
+      sum += roman;
+  }
+  return sum;
 };
