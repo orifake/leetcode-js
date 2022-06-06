c3630c40f0ecbbc1d5876af2483766bbc2546ab3
orifake <349758699@qq.com>
2022-06-07 00:09:12 +0800

    update: Power of Two

--- Power of Two.js
+++ Power of Two.js
@@ -1,7 +1,13 @@
 /**
+ *  Time: O(1)
+ *  Space: O(1)
+ */
+
+
+/**
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfTwo = function(n) {
+const isPowerOfTwo = function (n) {
     return (n > 0) && ((n & (n - 1)) === 0);
 };
