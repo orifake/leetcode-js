a884284f5cd88c42f38b14c7b7febaf7390a54cf
orifake <349758699@qq.com>
2022-06-28 22:12:56 +0800

    add: Keyboard Row

---
+++ Keyboard Row.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = function (words) {
+  const keyboard = 'qwertyuiopasdfghjklzxcvbnm'
+  let ans = []
+  for (let word of words) {
+    if (word.length === 1) {
+      ans.push(word)
+      continue
+    }
+    let prev = -1
+    let curr = -1
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i]
+      const index = keyboard.indexOf(char.toLocaleLowerCase())
+      if (index < 10) {
+        curr = 0
+      } else if (index < 19) {
+        curr = 1
+      } else {
+        curr = 2
+      }
+
+      if (i === 0) {
+        prev = curr
+      } else if (prev !== curr) {
+        break
+      } else if (i === word.length - 1) {
+        ans.push(word)
+      }
+    }
+  }
+  return ans
+};
+
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]))
