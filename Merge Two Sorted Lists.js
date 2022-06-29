dfc81d582ab6db9a5bd2e33e59054b6645d0cee3
orifake <349758699@qq.com>
2022-06-30 00:53:51 +0800

    update: Merge Two Sorted Lists

--- Merge Two Sorted Lists.js
+++ Merge Two Sorted Lists.js
@@ -10,23 +10,26 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    var result = [];
-    while (l1) {
-        result.push(new ListNode(l1.val))
-        l1 = l1.next;
+const mergeTwoLists = function (l1, l2) {
+  if (!l1) return l2
+  if (!l2) return l1
+  let dummy = new ListNode()
+  let head = dummy
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      head.next = l2
+      l2 = l2.next
+    } else {
+      head.next = l1
+      l1 = l1.next
     }
-    while (l2) {
-        result.push(new ListNode(l2.val))
-        l2 = l2.next;
-    }
+    head = head.next
+  }
+  if (l1) {
+    head.next = l1
+  } else {
+    head.next = l2
+  }
 
-    result.sort(function(a, b) {
-        return a.val - b.val
-    })
-
-    for (var i = 0, len = result.length; i < len - 1; i++)
-        result[i].next = result[i + 1];
-
-    return result.length ? result[0] : null;
+  return dummy.next
 };
