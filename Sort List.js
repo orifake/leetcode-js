f3d913af649bf3cbb1709cb01b1d3d469c94a14b
orifake <349758699@qq.com>
2022-07-05 23:09:33 +0800

    update: Sort List

--- Sort List.js
+++ Sort List.js
@@ -4,54 +4,52 @@
  */
 
 const sortList = function (head) {
-	//If List Contain a Single or 0 Node
-	if (!head || !head.next) return head
+  //If List Contain a Single or 0 Node
+  if (!head || !head.next) return head
 
-	let fast = head;
-	let slow = head;
-	let temp = null
+  let fast = head;
+  let slow = head;
+  let temp = null
 
-	// 2 pointer appraoach / turtle-hare Algorithm (Finding the middle element)
-	while (fast && fast.next) {
-		temp = slow;
-		slow = slow.next;       //slow increment by 1
-		fast = fast.next.next;  //fast incremented by 2
-	}
+  // 2 pointer appraoach / turtle-hare Algorithm (Finding the middle element)
+  while (fast && fast.next) {
+    temp = slow;
+    slow = slow.next;       //slow increment by 1
+    fast = fast.next.next;  //fast incremented by 2
+  }
 
-	temp.next = null          //end of first left half
-	const l1 = sortList(head)
-	const l2 = sortList(slow)
-	return mergeList(l1, l2)
+  temp.next = null          //end of first left half
+  const l1 = sortList(head)
+  const l2 = sortList(slow)
+  return mergeList(l1, l2)
 };
 
 //MergeSort Function O(n*logn)
 function mergeList(l1, l2) {
-	let ptr = new ListNode();
-	let curr = ptr;
+  let head = new ListNode();
+  let curr = head;
 
-	while (l1 && l2) {
-		if (l1.val <= l2.val) {
-			curr.next = l1;
-			l1 = l1.next;
-		} else {
-			curr.next = l2;
-			l2 = l2.next;
-		}
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
 
-		curr = curr.next;
-	}
+  //for unqual length linked list
 
-	//for unqual length linked list
+  if (l1) {
+    curr.next = l1;
+    l1 = l1.next;
+  }
 
-	if (l1) {
-		curr.next = l1;
-		l1 = l1.next;
-	}
-
-	if (l2) {
-		curr.next = l2;
-		l2 = l2.next;
-	}
-
-	return ptr.next;
+  if (l2) {
+    curr.next = l2;
+    l2 = l2.next;
+  }
+  return head.next;
 }
