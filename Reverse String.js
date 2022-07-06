c50f735dbb441c7a3ccfb10e1616c9b9aa7f8cb6
orifake <349758699@qq.com>
2022-07-07 00:21:15 +0800

    update: Reverse String

--- Reverse String.js
+++ Reverse String.js
@@ -2,10 +2,15 @@
  * @param {string} s
  * @return {string}
  */
-var reverseString = function(s) {
-     if (!s) {
-        return s;
-    }
-
-    return s.split('').reverse().join('');
+const reverseString = function (s) {
+  let left = 0
+  let right = s.length - 1
+  while (left < right) {
+    // 交换 s[left] 和 s[right]
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
 };
