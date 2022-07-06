f5d5d63137dbb60f187b0d9b3f1fe078a20e4cc3
orifake <349758699@qq.com>
2022-07-07 00:01:51 +0800

    update: Move Zeroes

--- Move Zeroes.js
+++ Move Zeroes.js
@@ -2,14 +2,22 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-	var length = nums.length;
-	for (var i = 0; i < length; i++) {
-		if (nums[i] === 0) {
-			nums.splice(i, 1);
-			nums.push(0);
-			length--;
-			i--;
-		}
+const moveZeroes = function (nums) {
+	let p = removeElement(nums, 0)
+	for (; p < nums.length; p++) {
+		nums[p] = 0;
 	}
 };
+
+const removeElement = function (nums, val) {
+	let fast = 0
+	let slow = 0
+	while (fast < nums.length) {
+		if (nums[fast] !== val) {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
