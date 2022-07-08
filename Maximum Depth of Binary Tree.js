116cb444d0ee32ad13e98508cba652e48233e35b
orifake <349758699@qq.com>
2022-07-08 23:30:28 +0800

    update: Maximum Depth of Binary Tree

--- Maximum Depth of Binary Tree.js
+++ Maximum Depth of Binary Tree.js
@@ -1,24 +1,17 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-	var max = -1;
-	var cal = function(node, depth) {
-		if (!node) {
-			max = Math.max(max, depth);
-			return
-		}
-		cal(node.left, depth + 1);
-		cal(node.right, depth + 1);
-	}
-	cal(root, 0);
-	return max;
+
+const maxDepth = function (root) {
+	if (!root) return 0
+	return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
