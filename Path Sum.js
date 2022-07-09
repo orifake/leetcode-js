f0f49af823e626f72f4e57e136fecb404b65b190
orifake <349758699@qq.com>
2022-07-09 22:52:51 +0800

    update: Path Sum

--- Path Sum.js
+++ Path Sum.js
@@ -10,23 +10,21 @@
  * @param {number} sum
  * @return {boolean}
  */
-var hasPathSum = function(root, sum) {
-	if (!root)
-		return false;
-
+const hasPathSum = function (root, sum) {
+	let a
+	let b
+	if (!root) return false;
 	if (!root.left && !root.right) {
 		if (root.val === sum) {
 			return true;
 		}
 		return false;
 	}
-
 	if (root.left) {
-		var a = hasPathSum(root.left, sum - root.val);
+		a = hasPathSum(root.left, sum - root.val);
 	}
 	if (root.right) {
-		var b = hasPathSum(root.right, sum - root.val);
+		b = hasPathSum(root.right, sum - root.val);
 	}
-
 	return Boolean(a || b);
 };
