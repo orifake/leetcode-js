6aafc65253304c1bd2669951c05520f381f9b4a9
orifake <349758699@qq.com>
2022-07-10 23:25:53 +0800

    add: Find Mode in Binary Search Tree

---
+++ Find Mode in Binary Search Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const findMode = function (root) {
+  const count = {}
+  const inOrder = function (node) {
+    if (!node) return
+    inOrder(node.left)
+    count[node.val] = (count[node.val] ? count[node.val] + 1 : 1)
+    inOrder(node.right)
+  }
+  inOrder(root)
+  const ans = []
+  const freq = Math.max(...Object.values(count))
+  for (const [key, value] of Object.entries(count)) {
+    (value === freq) && ans.push(key)
+  }
+  return ans
+};
