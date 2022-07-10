f10601b1a3f279400a06cbe4291ca1ff412511a4
orifake <349758699@qq.com>
2022-07-10 22:31:59 +0800

    update: Invert Binary Tree

--- Invert Binary Tree.js
+++ Invert Binary Tree.js
@@ -1,25 +1,20 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-	if (!root) {
-		return root
-	}
-	var left = root.left;
-	var right = root.right;
-	left && invertTree(left);
-	right && invertTree(right);
+const invertTree = function (root) {
+	if (!root) return root
+	root.left && invertTree(root.left);
+	root.right && invertTree(root.right);
 
-	root.left = right;
-	root.right = left;
-
+	[root.left, root.right] = [root.right, root.left]
 	return root
 };
