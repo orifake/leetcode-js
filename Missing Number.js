4ed83f86cc29313869916d33d030e5fd858a5315
orifake <349758699@qq.com>
2022-07-13 22:01:10 +0800

    update: Missing Number

--- Missing Number.js
+++ Missing Number.js
@@ -2,12 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-	var max = Math.max.apply(null, nums);
-	var min = Math.min.apply(null, nums);
-
-	if (min !== 0) return 0;
-	return ((max * (max + 1) / 2) - nums.reduce(function(prev, next) {
-		return prev + next;
-	})) || max + 1
+const missingNumber = function (nums) {
+	const expected_sum = ~~(nums.length * (nums.length + 1) / 2)
+	const actual_sum = nums.reduce((partialSum, a) => partialSum + a, 0)
+	return expected_sum - actual_sum
 };
