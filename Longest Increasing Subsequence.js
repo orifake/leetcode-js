abc5796c86cd5c42076dd3e05edf671220098eff
orifake <349758699@qq.com>
2022-07-14 22:40:19 +0800

    update: Longest Increasing Subsequence

--- Longest Increasing Subsequence.js
+++ Longest Increasing Subsequence.js
@@ -2,23 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
-	var i, j;
-	var temp = [];
-	var len = nums.length;
-	var max = 1;
-
-	if (len === 0 || len === 1) {
-		return len;
-	}
-	for (i = 0; i < len; i++) {
-		temp[i] = 1;
-		for (j = 0; j < i; j++) {
-			if ((nums[i] > nums[j]) && (temp[j] >= temp[i])) {
-				temp[i] = temp[j] + 1;
-			}
-			max = Math.max(max, temp[i])
+const lengthOfLIS = function (nums) {
+	const lis = [];
+	for (let i = 0; i < nums.length; i++) {
+		lis.push(1);
+		for (let j = 0; j < i; j++) {
+			if (nums[j] < nums[i]) lis[i] = Math.max(lis[i], lis[j] + 1);
 		}
 	}
-	return max
+	return nums.length ? Math.max.apply(null, lis) : 0;
 };
