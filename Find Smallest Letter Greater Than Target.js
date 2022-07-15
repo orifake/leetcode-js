b0959e8de9b71dacf8d3a55cd17919bbb4010132
orifake <349758699@qq.com>
2022-07-15 12:35:39 +0800

    add: Find Smallest Letter Greater Than Target

---
+++ Find Smallest Letter Greater Than Target.js
@@ -0,0 +1,24 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+const nextGreatestLetter = function (letters, target) {
+  if (letters[0] > target || target >= letters[letters.length - 1]) {
+    return letters[0]
+  }
+
+  let left = 0
+  let right = letters.length - 1
+  while (left < right) {
+    let mid = parseInt((left + right) / 2)
+    if (letters[mid] <= target) {
+      left = mid + 1
+    } else {
+      right = mid
+    }
+  }
+  return letters[left]
+};
+
+console.log(nextGreatestLetter(["c", "f", "j"], "c"))
