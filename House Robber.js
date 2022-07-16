a92810a28ccfb41d4a67bfb0602027ea17c07581
orifake <349758699@qq.com>
2022-07-16 20:59:42 +0800

    update: House Robber

--- House Robber.js
+++ House Robber.js
@@ -2,22 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-	var length = nums.length;
-	var result = [];
-
-	if (length === 0) {
-		return 0;
+const rob = function (nums) {
+	let last = 0
+	let now = 0
+	for (num of nums) {
+		[last, now] = [now, Math.max(last + num, now)]
 	}
-
-	if (length === 1) {
-		return nums[0];
-	}
-
-	result[0] = nums[0];
-	result[1] = Math.max(nums[1], nums[0]);
-	for (var i = 2; i < length; i++) {
-		result[i] = Math.max(result[i - 2] + nums[i], result[i - 1]);
-	}
-	return result[length - 1];
+	return now
 };
+
+console.log(rob([2, 7, 9, 3, 1]))
