752e460b78d10b51667831511073550313ee72e9
orifake <349758699@qq.com>
2023-09-27 06:57:51 +0000

    add: Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+    if (preorder.length === 0) return null;
+    if (preorder.length === 1) return new TreeNode(preorder[0]);
+    const val = preorder[0];
+    const index = inorder.indexOf(val);
+    const leftInorder = inorder.slice(0, index);
+    const rightInorder = inorder.slice(index + 1);
+    const leftPreorder = preorder.slice(1, index + 1);
+    const rightPreorder = preorder.slice(index + 1);
+    const root = new TreeNode(val);
+    root.left = buildTree(leftPreorder, leftInorder);
+    root.right = buildTree(rightPreorder, rightInorder);
+    return root;
+};
