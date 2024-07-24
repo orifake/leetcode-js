5866351f00122cf1a72fe22558a256a5d0666193
orifake <349758699@qq.com>
2024-07-25 00:06:52 +0800

    update: Linked List Cycle II

--- 142.Linked List Cycle II.js
+++ 142.Linked List Cycle II.js
@@ -1,3 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
 const detectCycle = function (head) {
   let fast = head
   let slow = head
