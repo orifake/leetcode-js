fa7d30d08d3a7dc6ce362c6994d39aee7c9ebf60
orifake <349758699@qq.com>
2024-07-26 23:16:02 +0800

    update: Next Greater Element I

--- Next Greater Element I.js
+++ Next Greater Element I.js
@@ -4,21 +4,21 @@
  * @return {number[]}
  */
 const nextGreaterElement = function (nums1, nums2) {
-  const m = {}
-  const st = []
-  const ans = []
+  const hashMap = {}
+  const stack = []
+  const result = []
   nums2.forEach((num) => {
-    while (st.length && st[st.length - 1] < num) {
-      m[st.pop()] = num
+    while (stack.length && stack[stack.length - 1] < num) {
+      hashMap[stack.pop()] = num
     }
-    st.push(num)
+    stack.push(num)
   })
 
   nums1.forEach((num) => {
-    ans.push(m[num] || -1)
+    result.push(hashMap[num] || -1)
   })
 
-  return ans
+  return result
 };
 
 console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
