415f005d4aa640891cc50dbf428405f5f2329230
orifake <349758699@qq.com>
2024-07-28 23:12:29 +0800

    update: Sqrt(x)

--- 69.Sqrt(x).js
+++ 69.Sqrt(x).js
@@ -5,13 +5,12 @@
 const mySqrt = function (x) {
   let start = 1
   let end = x
-  let mid;
   if (x === 0) {
     return 0;
   }
 
   while (start + 1 < end) {
-    mid = start + parseInt((end - start) / 2);
+    let mid = start + parseInt((end - start) / 2);
     if (mid * mid <= x) {
       start = mid;
     } else {
