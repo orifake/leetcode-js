8d33e301cc411dac4ca61309c2d571cc4988e386
orifake <349758699@qq.com>
2024-07-28 12:36:15 +0800

    update: Number of Segments in a String

--- 434.Number of Segments in a String.js
+++ 434.Number of Segments in a String.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 const countSegments = function (s) {
-  let ans = Number(s.length && s[s.length - 1] !== ' ')
+  let result = Number(s.length && s[s.length - 1] !== ' ')
   for (let i = 1; i < s.length; i++) {
     if (s[i] === ' ' && s[i - 1] !== ' ') {
-      ans += 1
+      result += 1
     }
   }
-  return ans
+  return result
 };
 
-console.log(countSegments("a, bb, cc, cc#, "))
+console.log(countSegments("a, bb, cc, cc#,"))
