08e5c694a67ab2557bde1909149effe140f0d632
orifake <349758699@qq.com>
2024-07-29 22:57:28 +0800

    update: Heaters

--- 475.Heaters.js
+++ 475.Heaters.js
@@ -9,18 +9,18 @@ const findRadius = function (houses, heaters) {
 };
 
 const findMinDistance = (house, heaters) => {
-  let left = 0;
-  let right = heaters.length - 1;
-  while (left <= right) {
-    const mid = left + ((right - left) >> 1);
+  let low = 0;
+  let high = heaters.length - 1;
+  while (low <= high) {
+    const mid = low + parseInt((high - low) / 2);
     if (heaters[mid] <= house && house <= heaters[mid + 1]) {
       return Math.min(house - heaters[mid], heaters[mid + 1] - house);
     } else if (heaters[mid] <= house) {
-      left = mid + 1;
+      low = mid + 1;
     } else {
-      right = mid - 1;
+      high = mid - 1;
     }
   }
-  if (left === 0) return heaters[0] - house;
-  if (left === heaters.length) return house - heaters[heaters.length - 1];
+  if (low === 0) return heaters[0] - house;
+  if (low === heaters.length) return house - heaters[heaters.length - 1];
 };
