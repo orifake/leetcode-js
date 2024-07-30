1b7c3eab2005d5ffe27ef6a72d31ac138d033991
orifake <349758699@qq.com>
2024-07-30 23:12:13 +0800

    update: Bulls and Cows

--- 299.Bulls and Cows.js
+++ 299.Bulls and Cows.js
@@ -6,17 +6,17 @@
 const getHint = function (secret, guess) {
   let bull = 0;
   let cow = 0;
-  const map = {};
+  const lookup = {};
   for (let i = 0; i < secret.length; i++) {
     const s = secret.charAt(i);
     const g = guess.charAt(i);
     if (s === g) {
       bull++;
     } else {
-      if (map[s] < 0) cow++;
-      if (map[g] > 0) cow++;
-      map[s] = parseInt(map[s] || '0') + 1;
-      map[g] = parseInt(map[g] || '0') - 1;
+      if (lookup[s] < 0) cow++;
+      if (lookup[g] > 0) cow++;
+      lookup[s] = parseInt(lookup[s] || '0') + 1;
+      lookup[g] = parseInt(lookup[g] || '0') - 1;
     }
   }
   return `${bull}A${cow}B`;
