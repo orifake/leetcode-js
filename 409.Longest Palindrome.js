0c897026ea1e354a5fdce2a9f66ed4a605d1205e
orifake <349758699@qq.com>
2024-07-30 23:33:04 +0800

    update: Longest Palindrome

--- 409.Longest Palindrome.js
+++ 409.Longest Palindrome.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 const longestPalindrome = function (s) {
-  let ans = 0
+  let result = 0
   let lookup = {}
   for (let char of s) {
     lookup[char] = (lookup[char] || 0) + 1
-    if (lookup[char] % 2 === 0) ans += 2
+    if (lookup[char] % 2 === 0) result += 2
   }
-  return s.length > ans ? ans + 1 : ans
+  return s.length > result ? result + 1 : result
 };
