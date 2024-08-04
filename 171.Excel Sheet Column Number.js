de3bf40b8596f060a1d1d82b5ecb0a98c2158448
orifake <349758699@qq.com>
2024-08-04 23:29:52 +0800

    update: Excel Sheet Column Number

--- 171.Excel Sheet Column Number.js
+++ 171.Excel Sheet Column Number.js
@@ -2,9 +2,9 @@
  * @param {string} s
  * @return {number}
  */
-var titleToNumber = function(s) {
-	var array = s.split('').reverse();
-	return array.reduce(function(prev, str, index) {
+const titleToNumber = function (s) {
+	const array = s.split('').reverse();
+	return array.reduce(function (prev, str, index) {
 		return prev + Math.pow(26, index) * (array[index].charCodeAt(0) - 64);
 	}, 0);
 };
