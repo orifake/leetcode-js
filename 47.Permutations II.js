c69287b9eaca1670188ac04986139749f407e378
orifake <349758699@qq.com>
2024-08-06 23:18:13 +0800

    add: PermutationsPermutations II

--- 47.Permutations II.js
+++ 47.Permutations II.js
@@ -2,33 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permute = function (nums) {
+const permuteUnique = function (nums) {
   let len = nums.length
   let result = []
   let track = []
-
   let used = Array(len).fill(false);
+
+  nums = nums.sort((a, b) => a - b);
 
   if (!len) return []
   const backtrack = () => {
-    if (track.length === len) {
+    if (track.length === nums.length) {
       result.push([...track])
       return
     }
-    for (let i = 0; i < len; i++) {
+
+    for (let i = 0; i < nums.length; i++) {
+      // 排除不合法的选择
       if (used[i]) {
         continue
       }
+      if (nums[i] === nums[i - 1] && !used[i - 1]) {
+        continue
+      }
+
       track.push(nums[i])
       used[i] = true
-      backtrack()
+      backtrack(nums, track)
       track.pop()
       used[i] = false
     }
   }
-  backtrack(track, used)
+  backtrack()
   return result
 }
 
 
-console.log(permute([1, 2, 3]))
+console.log(permute([1, 1, 2]))
