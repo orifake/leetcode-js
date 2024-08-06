9b00f3ebe6543bbb956598a640390483ce012117
orifake <349758699@qq.com>
2024-08-06 23:41:38 +0800

    add: Next Permutation

---
+++ 31.Next Permutation.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const nextPermutation = function (nums) {
+  let pivot = -1
+  for (let i = nums.length - 1; i > 0; i--) {
+    if (nums[i] > nums[i - 1]) {
+      pivot = i - 1
+      break
+    }
+  }
+
+  if (pivot === -1) {
+    nums.reverse()
+    return
+  }
+
+  for (let i = nums.length - 1; i > pivot; i--) {
+    if (nums[i] > nums[pivot]) {
+      [nums[i], nums[pivot]] = [nums[pivot], nums[i]]
+      break
+    }
+  }
+
+  for (let i = pivot + 1, j = nums.length - 1; i < j; i++, j--) {
+    [nums[i], nums[j]] = [nums[j], nums[i]]
+  }
+}
+
+nextPermutation([3, 2, 1])
