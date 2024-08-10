c50d1d49d1960b3a41f15d7d0fa5a02ee79c78b6
shuizai <admin@admindeMacBook-Pro.local>
2024-08-10 21:54:22 +0800

    add: Combination Sum

---
+++ 39.Combination Sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum = function (candidates, target) {
+    const result = [];
+
+    function backtrack(index, comb, total) {
+        if (total === target) {
+            result.push([...comb]);
+            return
+        }
+
+        if (total > target || index >= candidates.length) {
+            return
+        }
+
+        comb.push(candidates[index])
+        backtrack(index, comb, total + candidates[index])
+        comb.pop()
+        backtrack(index + 1, comb, total)
+    }
+
+    backtrack(0, [], 0)
+    return result
+};
+
+console.log(combinationSum([2, 3, 5], 8))
