26a00de4ad1a4cc2da9294dafd331f799a2f8f26
shuizai <timyjykaka@gmail.com>
2024-08-16 22:09:10 +0800

    add: Number of Islands

---
+++ 200.Number of Islands.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const numIslands = function (grid) {
+  if (!grid.length || !grid[0].length) {
+    return 0
+  }
+  const rows = grid.length
+  const cols = grid[0].length
+  let islands = 0
+
+  const dfs = (row, col) => {
+    if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !== '1') return
+    grid[row][col] = '0'
+
+    dfs(row - 1, col)
+    dfs(row + 1, col)
+    dfs(row, col - 1)
+    dfs(row, col + 1)
+  }
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (grid[row][col] === '1') {
+        dfs(row, col)
+        islands++
+      }
+    }
+  }
+  return islands
+};
+
+console.log(numIslands([
+  ["1", "1", "1", "1", "0"],
+  ["1", "1", "0", "1", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"]
+]))
