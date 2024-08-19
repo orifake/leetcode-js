ead4353aabe34a0ec0c662b15c8d540ee2d1fec8
shuizai <timyjykaka@gmail.com>
2024-08-19 21:02:49 +0800

    add: H-Index

---
+++ 274.H-Index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+const hIndex = function (citations) {
+  const n = citations.length
+  const count = new Array(n + 1).fill(0)
+
+  for (let c of citations) {
+    if (c >= n) {
+      count[n]++
+    } else {
+      count[c]++
+    }
+  }
+
+  let total = 0
+  for (let i = n; i >= 0; i--) {
+    total += count[i]
+    if (total >= i) {
+      return i
+    }
+  }
+
+  return 0;
+};
