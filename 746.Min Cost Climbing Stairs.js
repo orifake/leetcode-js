880cd5c1880b793573efbc9532d07b7f55757108
orifake <timyjykaka@gmail.com>
2024-08-31 15:45:23 +0800

    update: Min Cost Climbing Stairs

--- 746.Min Cost Climbing Stairs.js
+++ 746.Min Cost Climbing Stairs.js
@@ -3,8 +3,17 @@
  * @return {number}
  */
 const minCostClimbingStairs = function (cost) {
-  for (let i = cost.length - 3; i >= 0; i--) {
-    cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+  let dp = 0
+  let dp_1 = 0
+  let dp_2 = 0
+
+  for (let i = 2; i <= cost.length; i++) {
+    dp = Math.min(dp_2 + cost[i - 2], dp_1 + cost[i - 1])
+    dp_2 = dp_1
+    dp_1 = dp
   }
-  return Math.min(cost[0], cost[1]);
+
+  return dp_1
 };
+
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
