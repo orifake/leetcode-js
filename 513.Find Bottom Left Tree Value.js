99f04dcf49c6bf3759db50c965a1ba143c4c6df7
shuizai <timyjykaka@gmail.com>
2024-09-01 23:31:11 +0800

    add: Find Bottom Left Tree Value

---
+++ 513.Find Bottom Left Tree Value.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const findBottomLeftValue = function (root) {
+
+  const queue = [root]
+  let result
+
+  while (queue.length) {
+
+    const node = queue.shift()
+    result = node.val
+
+    if (node.right) {
+      queue.push(node.right)
+    }
+    if (node.left) {
+      queue.push(node.left)
+    }
+
+  }
+  return result
+};
