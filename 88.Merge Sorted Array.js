f73a9f249966647a02c49f7c47a7c232ae0c41a9
orifake <timyjykaka@gmail.com>
2025-06-04 20:26:25 +0800

    update

--- 88.Merge Sorted Array.js
+++ 88.Merge Sorted Array.js
@@ -25,3 +25,10 @@ const merge = function (nums1, m, nums2, n) {
     p--
   }
 }
+
+const nums1 = [1, 2, 3, 0, 0, 0]
+const m = 3
+const nums2 = [2, 5, 6] 
+const n = 3
+merge(nums1, m, nums2, n)
+console.log(nums1) // Output: [1, 2, 2, 3, 5, 6]
