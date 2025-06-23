1478d46d2601491dbdb58a5903cfe7c99045cc49
shuizai <timyjykaka@gmail.com>
2025-06-23 23:54:18 +0800

    update: Maximum Depth of Binary Tree

--- 104.Maximum Depth of Binary Tree.js
+++ 104.Maximum Depth of Binary Tree.js
@@ -20,3 +20,22 @@ const maxDepth = function (root) {
   if (!root) return 0
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
+
+const maxDepth2 = function (root) {
+  let depth = 0;
+  let result = 0;
+
+  const traverse = (node) => {
+    if (!node) return;
+    depth++;
+    if (!node.left && !node.right)  {
+      // If it's a leaf node, update the result
+      result = Math.max(result, depth);
+    }
+    traverse(node.left);
+    traverse(node.right);
+    depth--;
+  };
+  traverse(root);
+  return result;
+}
