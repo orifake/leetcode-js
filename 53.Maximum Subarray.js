f0addaed09ef9297490d66d8191c90a1403c6313
orifake <timyjykaka@gmail.com>
2025-06-24 15:41:05 +0800

    add: Maximum Subarray

--- 53.Maximum Subarray.js
+++ 53.Maximum Subarray.js
@@ -6,14 +6,10 @@ const maxSubArray = function (nums) {
   if (!nums.length) return 0
   let dp = []
   dp[0] = nums[0]
-  let ans = Number.NEGATIVE_INFINITY
   for (let i = 1; i < nums.length; i++) {
     dp[i] = Math.max(nums[i], nums[i] + dp[i - 1])
   }
-  for (let j = 0; j < nums.length; j++) {
-    ans = Math.max(ans, dp[j])
-  }
-  return ans
+  return Math.max.apply(null, dp);
 };
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
