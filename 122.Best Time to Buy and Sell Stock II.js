5f7f1378e34fbf9711d93adfb17e58e79bbb156a
orifake <timyjykaka@gmail.com>
2025-06-24 16:28:52 +0800

    add: Best Time to Buy and Sell Stock II

--- 122.Best Time to Buy and Sell Stock II.js
+++ 122.Best Time to Buy and Sell Stock II.js
@@ -5,7 +5,11 @@
 const maxProfit = function (prices) {
   let ans = 0
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] - prices[i - 1] > 0) ans += prices[i] - prices[i - 1]
+    if (prices[i] - prices[i - 1] > 0) {
+      ans += prices[i] - prices[i - 1]
+    }
   }
   return ans
 };
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 7
