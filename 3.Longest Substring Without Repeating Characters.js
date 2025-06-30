c215921adfa8bf189b7dc99630d28a8dc648c7b8
orifake <timyjykaka@gmail.com>
2025-06-30 17:14:20 +0800

    update: Longest Substring Without Repeating Characters

--- 3.Longest Substring Without Repeating Characters.js
+++ 3.Longest Substring Without Repeating Characters.js
@@ -3,17 +3,24 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function (s) {
-  const lookup = {}
-  let result = 0
-  let left = 0
-  for (let right = 0; right < s.length; right++) {
-    if (s[right] in lookup) {
-      left = Math.max(left, lookup[s[right]] + 1)
+  let window = new Map();
+  let left = 0;
+  let right = 0;
+  let result = 0;
+  while (right < s.length) {
+    const char = s[right];
+    right++;
+    window.set(char, (window.get(char) || 0) + 1);
+
+    while (window.get(char) > 1) {
+      const leftChar = s[left];
+      left++;
+      window.set(leftChar, window.get(leftChar) - 1);
     }
-    lookup[s[right]] = right
-    result = Math.max(result, right - left + 1)
+
+    result = Math.max(result, right - left);
   }
-  return result
+  return result;
 };
 
-console.log(lengthOfLongestSubstring("abcabcbb"))
+console.log(lengthOfLongestSubstring("abcabcbb")) // Output: 3
